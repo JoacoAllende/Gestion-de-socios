319f45f517235661063b2f4c261301eb1e05c82e
Joaquin Allende <joacoallendeids@gmail.com>
2025-08-28 15:22:07 -0300

    Add initial configuration BE

---
+++ Server/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const cors = require('cors');
+var compression = require('compression');
+var helmet = require('helmet');
+
+require('./database');
+
+// Settings
+
+app.set('port', process.env.PORT || 3000);
+
+// Middlewares
+
+app.use(helmet());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(cors({}))
+
+// Routes
+
+app.use(compression());
+
+// Starting the server
+
+app.listen(app.get('port'), () => {
+    console.log('Server on port ', app.get('port'));
+})
