07856d6a35435d20e5c2c5521ce77d7778528d21
Joaquin Allende <joacoallendeids@gmail.com>
2025-08-28 15:20:27 -0300

    Add common AG Table component

---
+++ gestionSocios/src/app/components/commons/ag-table/ag-table.component.ts
@@ -0,0 +1,32 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import {
+  AllCommunityModule,
+  ColDef, ColGroupDef, GridApi, GridReadyEvent, ModuleRegistry
+} from 'ag-grid-community';
+import { AgGridAngular } from 'ag-grid-angular';
+
+ModuleRegistry.registerModules([AllCommunityModule]);
+
+@Component({
+  selector: 'app-ag-table',
+  standalone: true,
+  imports: [AgGridAngular],
+  templateUrl: './ag-table.component.html',
+  styleUrls: ['./ag-table.component.scss']
+})
+export class AgTableComponent {
+  public gridApi!: GridApi;
+
+  @Input() rowData: any[] = [];
+  @Input() colDefs: (ColDef | ColGroupDef)[] = [];
+  @Input() defaultColDef: ColDef = { flex: 1, minWidth: 100, resizable: true };
+  @Input() pinnedBottomRowData: any[] = [];
+
+  @Output() gridReadyEvent = new EventEmitter<GridReadyEvent>();
+
+
+  onGridReady(params: GridReadyEvent) {
+    this.gridApi = params.api;
+    this.gridReadyEvent.emit(params);
+  }
+}
