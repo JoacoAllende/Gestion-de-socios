693ef842dc65dbef2fefae7915360d67854f6404
Joaquin Allende <joacoallendeids@gmail.com>
2025-08-28 15:18:00 -0300

    Add initial files FE

---
+++ gestionSocios/src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { MembershipPaymentsComponent } from './components/membership-payments/membership-payments.component';
+
+export const routes: Routes = [
+    { path: '', component: MembershipPaymentsComponent }
+];
