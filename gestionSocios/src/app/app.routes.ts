21c4287080c3510a90f75c4ba79299440a3d397c
Joaquin Allende <joacoallendeids@gmail.com>
2025-08-29 16:09:35 -0300

    Add FE membership implementation

--- gestionSocios/src/app/app.routes.ts
+++ gestionSocios/src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { MembershipPaymentsComponent } from './components/membership-payments/membership-payments.component';
+import { MembershipComponent } from './components/membership/membership.component';
 
 export const routes: Routes = [
-    { path: '', component: MembershipPaymentsComponent }
+    { path: '', component: MembershipPaymentsComponent },
+    { path: 'socio', component: MembershipComponent },
+    { path: 'socio/:id', component: MembershipComponent }
 ];
