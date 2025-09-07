691e9e7fe273de3a7d6e2f5ad81e87f24f1f2936
Joaquin Allende <joacoallendeids@gmail.com>
2025-09-07 11:56:37 -0300

    Fix update totals on first render

--- gestionSocios/src/app/components/full-statistics/full-statistics.component.ts
+++ gestionSocios/src/app/components/full-statistics/full-statistics.component.ts
@@ -144,7 +144,7 @@ export class FullStatisticsComponent implements OnInit {
 
   onGridReady(event: GridReadyEvent) {
     this.gridApi = event.api;
-    this.updateVisibleTotals();
+    this.gridApi.addEventListener('firstDataRendered', () => this.updateVisibleTotals());
     this.gridApi.addEventListener('filterChanged', () => this.updateVisibleTotals());
     this.gridApi.addEventListener('sortChanged', () => this.updateVisibleTotals());
   }
