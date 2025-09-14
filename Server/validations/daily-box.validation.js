689cff1125dc235e05bb3e8af94646e2df48ea7b
Joaquin Allende <joacoallendeids@gmail.com>
2025-09-14 15:21:53 -0300

    Add BE for daily box mevements

---
+++ Server/validations/daily-box.validation.js
@@ -0,0 +1,19 @@
+const dailyBoxValidator = {};
+const jwt = require('jsonwebtoken');
+const SECRET_KEY = process.env.JWT_SECRET || 'TheSecretKey';
+const dailyBoxController = require('../controllers/daily-box.controller');
+
+dailyBoxValidator.validate_getDailyBox = (req, res) => {
+    verifyTokenAndExecute(req, res, dailyBoxController.getDailyBox);
+}
+
+function verifyTokenAndExecute(req, res, nextFn) {
+    jwt.verify(req.token, SECRET_KEY, (err) => {
+        if (err) {
+            return res.sendStatus(403);
+        }
+        nextFn(req, res);
+    });
+}
+
+module.exports = dailyBoxValidator;
