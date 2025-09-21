08b9249d17243cd43c168d3af6aee7dde01cf37d
Joaquin Allende <joacoallendeids@gmail.com>
2025-09-21 11:56:19 -0300

    Add BE for update employee

--- Server/routes/employees.routes.js
+++ Server/routes/employees.routes.js
@@ -4,7 +4,9 @@ const router = express.Router();
 const employees = require("../validations/employees.validation");
 
 router.get('/employees', ensureToken, employees.validate_getEmployees);
+router.get('/employee/:id', ensureToken, employees.validate_getEmployeeById);
 router.post('/employee', ensureToken, employees.validate_createEmployee);
+router.put('/employee', ensureToken, employees.validate_updateEmployee);
 
 function ensureToken(req, res, next) {
     const bearerHeader = req.headers['authorization'];
