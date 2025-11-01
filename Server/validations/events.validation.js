3ac1e1bd34a4304ec36c87020dfb1ccf529d7899
Joaquin Allende <joacoallendeids@gmail.com>
2025-11-01 15:08:44 -0300

    Add BE for events movements and details

--- Server/validations/events.validation.js
+++ Server/validations/events.validation.js
@@ -19,6 +19,38 @@ eventsValidator.validate_updateEvent = (req, res) => {
     verifyTokenAndExecute(req, res, eventsController.updateEvent);
 }
 
+eventsValidator.validate_getMovementsByEvent = (req, res) => {
+    verifyTokenAndExecute(req, res, eventsController.getMovementsByEvent);
+}
+
+eventsValidator.validate_createMovement = (req, res) => {
+    verifyTokenAndExecute(req, res, eventsController.createMovement);
+}
+
+eventsValidator.validate_updateMovement = (req, res) => {
+    verifyTokenAndExecute(req, res, eventsController.updateMovement);
+}
+
+eventsValidator.validate_deleteMovement = (req, res) => {
+    verifyTokenAndExecute(req, res, eventsController.deleteMovement);
+}
+
+eventsValidator.validate_getDetailsByMovement = (req, res) => {
+    verifyTokenAndExecute(req, res, eventsController.getDetailsByMovement);
+}
+
+eventsValidator.validate_createDetail = (req, res) => {
+    verifyTokenAndExecute(req, res, eventsController.createDetail);
+}
+
+eventsValidator.validate_updateDetail = (req, res) => {
+    verifyTokenAndExecute(req, res, eventsController.updateDetail);
+}
+
+eventsValidator.validate_deleteDetail = (req, res) => {
+    verifyTokenAndExecute(req, res, eventsController.deleteDetail);
+}
+
 function verifyTokenAndExecute(req, res, nextFn) {
     jwt.verify(req.token, SECRET_KEY, (err) => {
         if (err) {
