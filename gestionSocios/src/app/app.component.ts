3bc98bd43d8ba3bb53a993a607ffafd0ae50c75e
Joaquin Allende <joacoallendeids@gmail.com>
2025-12-13 14:12:24 -0300

    Add global spinner

--- gestionSocios/src/app/app.component.ts
+++ gestionSocios/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DynamicMenuComponent } from './components/dynamic-menu/dynamic-menu.component';
 import { ToastComponent } from './components/commons/toast/toast.component';
+import { SpinnerComponent } from './components/commons/spinner/spinner.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, DynamicMenuComponent, ToastComponent],
+  imports: [RouterOutlet, DynamicMenuComponent, ToastComponent, SpinnerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
