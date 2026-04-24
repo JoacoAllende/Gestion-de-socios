35e3b5d317b18176d4f1b175a422b0e3125d0145
Joaquin Allende <joacoallendeids@gmail.com>
2026-04-24 20:48:06 -0300

    Protect login and register

--- Server/routes/user.routes.js
+++ Server/routes/user.routes.js
@@ -1,8 +1,30 @@
 const express = require('express');
+const rateLimit = require('express-rate-limit');
 const router = express.Router();
 const user = require('../validations/user.validation');
 
-router.post('/register', user.validate_registerUser);
-router.post('/login', user.validate_loginUser)
+const loginLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 10,
+    standardHeaders: true,
+    legacyHeaders: false,
+    skipSuccessfulRequests: true,
+    message: { error: 'Demasiados intentos de login. Intente de nuevo en 15 minutos.' }
+});
+
+router.post('/register', ensureToken, user.validate_registerUser);
+router.post('/login', loginLimiter, user.validate_loginUser)
+
+function ensureToken(req, res, next) {
+    const bearerHeader = req.headers['authorization'];
+    if (typeof bearerHeader !== 'undefined') {
+        const bearer = bearerHeader.split(' ');
+        const bearerToken = bearer[1];
+        req.token = bearerToken;
+        next();
+    } else {
+        res.sendStatus(403);
+    }
+}
 
 module.exports = router;
--- Server/validations/user.validation.js
+++ Server/validations/user.validation.js
@@ -1,13 +1,25 @@
 const userValidator = {};
+const jwt = require('jsonwebtoken');
+const SECRET_KEY = process.env.JWT_SECRET;
+if (!SECRET_KEY) throw new Error('JWT_SECRET no configurado');
 
 const userController = require('../controllers/user.controller');
 
 userValidator.validate_registerUser = (req, res) => {
-    userController.registerUser(req, res);
+    verifyTokenAndExecute(req, res, userController.registerUser);
 }
 
 userValidator.validate_loginUser = (req, res) => {
     userController.loginUser(req, res);
+}
+
+function verifyTokenAndExecute(req, res, nextFn) {
+    jwt.verify(req.token, SECRET_KEY, (err, decoded) => {
+        if (err) {
+            return res.sendStatus(403);
+        }
+        nextFn(req, res);
+    });
 }
 
 module.exports = userValidator;
